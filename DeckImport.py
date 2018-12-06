34824e619408783bfda175fc7389f1406beabd39
Nick Neuman <thisisnickadam@gmail.com>
2018-12-05 16:48:39 -0800

    CSV is parsed and images saved to ouput dir.

--- DeckImport.py
+++ DeckImport.py
@@ -1,5 +1,57 @@
-import sys
+import requests
+import re
+import os.path
 
 class DeckImport:
     def __init__(self, input, output):
-        print(input, output)
+
+        # Verify csv exists
+        srcExists = os.path.isfile(input)
+
+        # Handle non existent csv
+        if(not srcExists):
+            print("CSV could not be found.")
+            print("Path: " + input + "")
+            exit()
+
+        # Verify output dir exists
+        dirExists = os.path.isdir(output)
+
+        # Handle non existent output dir
+        if(not dirExists):
+            print("Output directory could not be found.")
+            print("Path: " + output + "")
+            exit()
+
+
+        # Get file object
+        srcFile = open(input, "r")
+
+        # Get individual lines
+        rows = srcFile.readlines()
+
+
+        # Remove and store property names
+        properties = rows.pop(0)
+
+        for row in rows:
+            splitRow = row.split(',')
+
+            orderNum = splitRow[0]
+
+            # Splitting adds newline at the end so the regex removes it
+            deckUrl = re.sub(r'[\n]', '', splitRow[1])
+        
+            response = requests.get(deckUrl)
+
+            print(response)
+
+            # Save image to output path
+            newPath = output + '/' + orderNum + '.png'
+
+            newImg = open(newPath, "w")
+
+            newImg.write(response.content)
+
+        srcFile.close()
+
--- app.py
+++ app.py
@@ -2,9 +2,9 @@ import sys
 from DeckImport import DeckImport
 
 # ANSI color codes
-RED = '\033[1;31;40m'
+RED = "\033[1;31;40m"
 GREEN = "\033[1;31;32m"
-RESET = '\033[0m'
+RESET = "\033[0m"
 
 args = sys.argv
 
@@ -15,18 +15,12 @@ input = None
 output = None
 
 # Handle lack of input
-if(len(args) < 2):
-    print(GREEN + "Invalid number of arguments." + RESET)
+if(len(args) < 3):
+    print(RED + "Invalid number of arguments." + RESET)
     exit()
 
 input = args[1]
 
-# Choose whether current file will be updated
-# or new file will be generated with updated paths
-if(len(args) > 2):
-    print(GREEN + "Creating new file for updated paths." + RESET)
-    output = args[2]
-else:
-    print(GREEN + "Updating current file with new values" + RESET)
+output = args[2]
 
 DeckImport(input, output)
