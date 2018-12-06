621e63aa0056e01443e43eb57754ce73cd844d07
neecholaus <thisisnickadam@gmail.com>
2018-12-05 21:37:38 -0800

    Added message for each photo is not retrieved, give count of successful requests at the end.

--- DeckImport.py
+++ DeckImport.py
@@ -5,18 +5,19 @@ from Colors import Colors
 from datetime import date
 import time
 
+
 class DeckImport:
 
     properties = None
     values = []
 
-    def writeImage(self, path, content):        
+    def writeImage(self, path, content):
         img = open(path, "w")
-        
+
         img.write(content)
 
     def updateCsv(self):
-        csvName = '/Users/nickneuman/downloads/decks-new.csv'
+        csvName = 'C:/Users/Nick/downloads/decks-new.csv'
 
         newFile = open(csvName, "w")
 
@@ -26,7 +27,6 @@ class DeckImport:
             string = ','.join(item) + '\n'
             newFile.write(string)
 
-
     def __init__(self, input, output):
 
         # Get file object
@@ -35,9 +35,10 @@ class DeckImport:
         # Get individual lines
         rows = srcFile.readlines()
 
-
         # Remove and store property names
         self.properties = rows.pop(0)
+
+        count = 0
 
         for row in rows:
             splitRow = row.split(',')
@@ -46,17 +47,17 @@ class DeckImport:
 
             # Splitting adds newline at the end so the regex removes it
             deckUrl = re.sub(r'[\n]', '', splitRow[1])
-        
+
             # Send request
             response = requests.get(deckUrl)
 
             if(response.status_code != 200):
-                # didn't get photo
-                # skip for now
+                print(Colors.RED + "404 not found: " + deckUrl + Colors.RESET)
                 continue
 
             # Create unique filename based on date and time
-            fileName = '/' + str(date.today()) + '-' + re.sub(r'[.]', '-', str(time.time())) + '.png'
+            fileName = '/' + str(date.today()) + '-' + \
+                re.sub(r'[.]', '-', str(time.time())) + '.png'
 
             path = output + fileName
 
@@ -66,7 +67,10 @@ class DeckImport:
 
             self.values.append(splitRow)
 
+            count += 1
+
+        print(Colors.GREEN + str(count) + " images were stored." + Colors.RESET)
+
         self.updateCsv()
 
         srcFile.close()
-
